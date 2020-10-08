890ac123ee2da145f4a2726adec5f860685bf79b
swastika <sunitamamgai@gmail.com>
2020-10-09 02:38:16 +0530

    second commit

--- app/routes.js
+++ app/routes.js
@@ -1,45 +1,34 @@
 const fs = require("fs");
 //const uniqueString = require("uniqe-string");
-var Contact = require("../app/models/contact.js");
-var Company = require("../app/models/company.js");
+const Contact = require("../app/models/contact.js");
+const Company = require("../app/models/company.js");
 
-module.exports = function(app){
+module.exports = function (app) {
     // Here we will write routes
 
-    app.get("/", function(req, res){
-        Contact.find({}, function(err, contactdata){
-            res.render("index.ejs",{
-                icontact: contactdata
+    app.get("/", function (req, res) {
+        var iCompany, iContacts;
+        Contact.find({}, function (err, contactData) {
+            iContacts = contactData;
+            Company.find({}, function (err, companyData) {
+                iCompany = companyData;
+                res.render("index.ejs", {
+                    icontact: iContacts,
+                    icompany: iCompany
+                });
             });
-            
-        });
-        
-    
-        
+        });  
     });
-    app.get("/", function(req, res){
-        Company.find({}, function(err, companydata){
-            res.render("index1.ejs",{
-                icompany: companydata
-            });
-            
-        });
-        
-    
-        
-    });
+
     //so this is get request to show contact page
-    app.get("/contact", function(req, res){
+    app.get("/contact", function (req, res) {
         res.render("contact.ejs")
     });
 
-    app.get("/company", function(req, res){
-        res.render("company.ejs")
-    });
 
     //now we will create post page to send data in database
 
-    app.post("/contact", function(req, res){
+    app.post("/contact", function (req, res) {
         var con = Contact();
 
         con.name = req.body.name;
@@ -47,29 +36,32 @@ module.exports = function(app){
         con.mobile = req.body.mobile;
         con.message = req.body.message;
 
-        con.save(function(err, getcontact){
-            if(err) res.json(err);
+        con.save(function (err, getcontact) {
+            if (err) res.json(err);
             else {
                 res.redirect("/");
             }
         });
-        
+
     });
-    app.post("/company", function(req, res){
+
+    app.get("/company", function (req, res) {
+        res.render("company.ejs")
+    });
+    app.post("/company", function (req, res) {
         var com = Company();
 
         com.name = req.body.name;
         com.companyname = req.body.companyname;
         com.address = req.body.address;
-        com.contactno = req.body.contact;
+        com.contactno = req.body.contactno;
 
-        com.save(function(err, getcompany){
-            if(err) res.json(err);
+        com.save(function (err, getcompany) {
+            if (err) res.json(err);
             else {
                 res.redirect("/");
             }
         });
-        
-    });
 
+    });
 }
